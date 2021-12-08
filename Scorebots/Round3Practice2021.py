44c1073803adc64278434876bc9a650999241dc1
CobblerOfShoes <78286646+CobblerOfShoes@users.noreply.github.com>
2021-12-08 16:14:25 -0500

    Update Round3Practice2021.py

--- Scorebots/Round3Practice2021.py
+++ Scorebots/Round3Practice2021.py
@@ -116,9 +116,9 @@ allTasks = [
 	Task('Returner','UFW does not deny incoming samba traffic', 3, '[ ! "$(grep dport /etc/ufw/after.rules)" ]'), #test
 	Task('Advance','UFW init file is executable', 5, '[ "$(stat -c %a /etc/init.d/ufw | grep 755)" ]'), #test
 		#Sysctl
-	Task('Returner','IPV4 TCP/IP SYN cookies enabled', 3, '[ "$(grep syncookies=1 /etc/sysctl.conf)" ]'), #test
-	Task('Returner','IPV4 ICMP redirets disabled', 3, '[ "$(grep "redirects = 0"/etc/sysctl.conf)" ]'), #test
-	Task('Returner','IPV4 logging of martian packets enabled', 3, '[ "$(grep "martians = 1" /etc/sysctl.conf)" ]'), #test
+	Task('Returner','IPV4 TCP/IP SYN cookies enabled', 3, '[ "$(grep -e syncookies=1 -e "syncookies = 1" /etc/sysctl.conf)" ]'), #test
+	Task('Returner','IPV4 ICMP redirets disabled', 3, '[ "$(grep -e "redirects = 0" -e "redirects=0" /etc/sysctl.conf)" ]'), #test
+	Task('Returner','IPV4 logging of martian packets enabled', 3, '[ "$(grep -e "martians = 1" -e "martians=1" /etc/sysctl.conf)" ]'), #test
 		#lightdm
 	]
 groups = [] #groups that must exist, or else a penalty
