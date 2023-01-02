964bb3ae5b17a683cb7b36d53d2a540a91e579b2
CobblerOfShoes <78286646+CobblerOfShoes@users.noreply.github.com>
2023-01-02 15:31:14 -0500

    Update startscoring.sh

--- CopyOnlyImages/CaseyImage/startscoring.sh
+++ CopyOnlyImages/CaseyImage/startscoring.sh
@@ -1,20 +1,20 @@
 #! /bin/bash
 
 sleep 10
-sudo apt install git
-sudo apt install shc
-sudo apt install gcc
+sudo dnf install git
+sudo dnf install shc
+sudo dnf install gcc
 git clone -b misc https://github.com/GreenLightning07/MadagascarLAMP.git
 chmod +x MadagascarLAMP/changes.sh
 chmod +x MadagascarLAMP/scorebot.sh
-mv MadagascarLAMP/scorebot.sh /var/local/scorebot.sh
-mv MadagascarLAMP/ScoreReport.html /home/skipper/Desktop/ScoreReport.html
-mv MadagascarLAMP/README.html /home/skipper/Desktop/README.html
-mv MadagascarLAMP/Contact.html /home/skipper/Desktop/Contact.html
-chown skipper:skipper /home/skipper/Desktop/ScoreReport.html
-chown skipper:skipper /home/skipper/Desktop/README.html
-chown skipper:skipper /home/skipper/Desktop/Contact.html
-MadagascarLAMP/changes.sh
+mv CyberStuff/CopyOnlyImages/CaseyImage/scorebot.sh /var/local/scorebot.sh
+mv CyberStuff/CopyOnlyImages/CaseyImage/ScoreReport.html /home/cyber/Desktop/ScoreReport.html
+mv CyberStuff/CopyOnlyImages/CaseyImage/README.html /home/cyber/Desktop/README.html
+mv CyberStuff/CopyOnlyImages/CaseyImage/Contact.html /home/cyber/Desktop/Contact.html
+chown cyber:cyber /home/cyber/Desktop/ScoreReport.html
+chown cyber:cyber /home/cyber/Desktop/README.html
+chown cyber:cyber /home/cyber/Desktop/Contact.html
+CyberStuff/CopyOnlyImages/CaseyImage/changes.sh
 shc -f /var/local/scorebot.sh
 rm /var/local/scorebot.sh
 sudo /var/local/scorebot.sh.x
